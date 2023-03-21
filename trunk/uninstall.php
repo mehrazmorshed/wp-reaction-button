9fc88361e2d90deacd87086e95240845f1722ac5
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-03-22 01:06:22 +0600

    Create uninstall.php

---
+++ trunk/uninstall.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * WP Reaction Button - uninstall script
+ *
+ * @package WPReactionButton
+ *
+ **/
+
+if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+  exit;
+}
+
+// uninstall script
