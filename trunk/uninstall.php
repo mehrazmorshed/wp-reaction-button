f6ec68930ca829fd8026efd7545c7148c5804627
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-03-24 23:45:04 +0600

    Update uninstall.php

--- trunk/uninstall.php
+++ trunk/uninstall.php
@@ -13,4 +13,13 @@ if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
   exit;
 }
 
+// uninstallation functions and hooks should be here
+
 // admin uninstallation feedback script
+
+/**
+ *
+ * third party APIs (such as: Freemium)
+ * can be used for receiving feedbacks
+ *
+ **/
