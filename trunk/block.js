c4419c6c7d62cdb1f8bc210892ee83211dc4b033
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-03-25 20:34:57 +0600

    Update block.js

--- trunk/block.js
+++ trunk/block.js
@@ -1,6 +1,6 @@
 /**
  *
- * WP Reaction Button Register Block
+ * WP Reaction Button Register Block Script
  * @package WPReactionButton
  * Author: Mehraz Morshed
  * Copyright 2023 WPDeveloper
@@ -12,7 +12,7 @@ wp.blocks.registerBlockType("ourplugin/wp-reaction-button", {
 	icon: "smiley",
 	category: "common",
 	edit: function () {
-		return wp.element.createElement("h6", null, "WP Reaction Button")
+		return wp.element.createElement("h4", null, "*** WP Reaction Button block added successfully. *** Please refresh the Front-end! *** Note: Using multiple WP Reaction Button elements / blocks / shortcodes in a single page may cause errors! ***")
 	},
 	save: function () {
 		return wp.element.createElement("h6", null, "[reactions]")
@@ -21,7 +21,7 @@ wp.blocks.registerBlockType("ourplugin/wp-reaction-button", {
 
 /**
  * 
- * I borrowed the few line of codes below from LearnWebCode Academy and
+ * I borrowed the few line of codes above from LearnWebCode Academy and
  * then re-writed according to my requirements, and I can explain it.
  * The copyright of original code goes to LearnWebCode Academy.
  * 
